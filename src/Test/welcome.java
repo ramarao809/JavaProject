e948f026a854c5562666729c23b4005caabedbbc
ramarao809 <36867040+ramarao809@users.noreply.github.com>
2023-11-29 16:19:38 -0500

    offer updated for ipHone 15 pro

--- src/Test/welcome.java
+++ src/Test/welcome.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	System.out.println("Welcome to the code lab technology");
 	System.out.println(30+50);
 	System.out.println("Successfully uploaded into github");
+	System.out.println("$100 discount on iPhone 15 pro this weekend sale");
 }
 }
               
