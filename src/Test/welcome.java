877b40cf26717e12b35fe921eae5f5003ea6221f
ramarao809 <36867040+ramarao809@users.noreply.github.com>
2023-11-29 16:11:53 -0500

    upload files to github

---
+++ src/Test/welcome.java
@@ -0,0 +1,9 @@
+package Test;
+
+public class welcome {
+public static void main(String[] args) {
+	System.out.println("Welcome to the code lab technology");
+	System.out.println(30+50);
+}
+}
+              
