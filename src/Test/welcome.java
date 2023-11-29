83e2337dd59b7087f60f60ba931bd15a5b317602
ramarao809 <36867040+ramarao809@users.noreply.github.com>
2023-11-29 16:16:28 -0500

    2nd commit

--- src/Test/welcome.java
+++ src/Test/welcome.java
@@ -4,6 +4,7 @@ public class welcome {
 public static void main(String[] args) {
 	System.out.println("Welcome to the code lab technology");
 	System.out.println(30+50);
+	System.out.println("Successfully uploaded into github");
 }
 }
               
